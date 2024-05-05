577f5cb4c44c85526b7083e6bad324efdc8aaa46
RyanCPotts <ryancpotts@gmail.com>
2024-05-05 16:19:45 -0500

    401 challenge 05

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -7,4 +7,25 @@ describe('Linked List', () => {
   it('works', () => {
     expect(true).toBeTruthy();
   });
-});
+
+
+  it('should insert a value', () => {
+    let sampleLL = new LinkedList();
+    sampleLL.insert('apple');
+    sampleLL.insert('mango');
+    sampleLL.insert('strawberry');
+    expect(sampleLL.head.value).toEqual('strawberry');
+    expect(sampleLL.head.next.value).toEqual('mango');
+
+
+  });
+
+  it('should include a value', () => {
+    let sampleLL = new LinkedList();
+    sampleLL.insert('apple');
+    sampleLL.insert('mango');
+    sampleLL.insert('strawberry');
+    expect(sampleLL.includes('mango')).toBe(true);
+  });
+}
+);
